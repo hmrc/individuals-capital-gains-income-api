10dc57bb7fa6ae9dacd984bfc5e547363c1d8c80
omotrium <omo4adex@gmail.com>
2026-01-21 15:40:55 +0000

    MTDSA-31710: fix of Error message - ITSALH000799 to be more descriptive

--- app/common/errors/CgtCommonErrors.scala
+++ app/common/errors/CgtCommonErrors.scala
@@ -51,8 +51,8 @@ object RuleIncorrectLossesSubmittedError
 
 object RuleIncorrectNonStandardGainsSubmittedError
     extends MtdError(
-      "RULE_INCORRECT_NON_STANDARD_GAINS_SUBMITTED",
-      "One of these fields must be provided: carriedInterestGain, attributedGains or otherGains",
+      "RULE_INCORRECT_NON_STANDARD_GAINS",
+      "At least one of carriedInterestGain, attributedGains or otherGains must be provided",
       BAD_REQUEST)
 
 object RuleDisposalDateErrorV1 extends MtdError("RULE_DISPOSAL_DATE", "The disposalDate must be within the specified tax year", BAD_REQUEST)
